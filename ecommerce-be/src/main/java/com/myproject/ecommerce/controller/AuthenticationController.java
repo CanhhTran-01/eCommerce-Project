d1b7b0fe261f86b338d754c11874aac32c1d1c30
Tran Duc Canh <levitran1102005@gmail.com>
2026-01-22 18:24:15 +0700

    fix(auth): add leading slash to request mapping

--- ecommerce-be/src/main/java/com/myproject/ecommerce/controller/AuthenticationController.java
+++ ecommerce-be/src/main/java/com/myproject/ecommerce/controller/AuthenticationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.text.ParseException;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
