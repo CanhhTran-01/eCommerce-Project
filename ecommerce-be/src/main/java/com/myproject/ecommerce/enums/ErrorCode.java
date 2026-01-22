a02d66e74fc25b8594a3cf3698436348ebc5e273
Tran Duc Canh <levitran1102005@gmail.com>
2026-01-23 03:41:20 +0700

    feat(error): add UNAUTHORIZED error code in ErrorCode enum

--- ecommerce-be/src/main/java/com/myproject/ecommerce/enums/ErrorCode.java
+++ ecommerce-be/src/main/java/com/myproject/ecommerce/enums/ErrorCode.java
@@ -10,12 +10,13 @@ import org.springframework.http.HttpStatusCode;
 public enum ErrorCode {
 
     UNCATEGORIZED_EXCEPTION(9999, "uncategorized error!", HttpStatus.INTERNAL_SERVER_ERROR),
-    USERNAME_EXISTED (1001, "username existed!", HttpStatus.BAD_REQUEST),
+    USERNAME_EXISTED(1001, "username existed!", HttpStatus.BAD_REQUEST),
     USERNAME_INVALID(1002, "username invalid!", HttpStatus.BAD_REQUEST),
     PASSWORD_INVALID(1003, "password invalid!", HttpStatus.BAD_REQUEST),
     EMAIL_INVALID(1004, "email invalid!", HttpStatus.BAD_REQUEST),
     ACCOUNT_NOT_FOUND(1005, "account not found!", HttpStatus.NOT_FOUND),
-    UNAUTHENTICATED(1006, "unauthenticated!", HttpStatus.UNAUTHORIZED)
+    UNAUTHENTICATED(1006, "unauthenticated!", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1007, "you have no permission with this page!", HttpStatus.FORBIDDEN)
     ;
 
 
