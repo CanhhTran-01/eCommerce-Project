08d84074e7748bd7d7595ce75de4bb0045788841
Tran Duc Canh <levitran1102005@gmail.com>
2026-01-22 22:01:14 +0700

    refactor(error): add http status and new error codes

--- ecommerce-be/src/main/java/com/myproject/ecommerce/enums/ErrorCode.java
+++ ecommerce-be/src/main/java/com/myproject/ecommerce/enums/ErrorCode.java
@@ -3,20 +3,23 @@ package com.myproject.ecommerce.enums;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
-    USERNAME_EXISTED (1001, "username existed!"),
-    USERNAME_INVALID(1002, "username invalid!"),
-    PASSWORD_INVALID(1003, "password invalid!"),
-    EMAIL_INVALID(1004, "email invalid!"),
-    ACCOUNT_NOT_FOUND(1005, "account not found!"),
-    UNAUTHENTICATED(1006, "unauthenticated!")
+    UNCATEGORIZED_EXCEPTION(9999, "uncategorized error!", HttpStatus.INTERNAL_SERVER_ERROR),
+    USERNAME_EXISTED (1001, "username existed!", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1002, "username invalid!", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1003, "password invalid!", HttpStatus.BAD_REQUEST),
+    EMAIL_INVALID(1004, "email invalid!", HttpStatus.BAD_REQUEST),
+    ACCOUNT_NOT_FOUND(1005, "account not found!", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1006, "unauthenticated!", HttpStatus.UNAUTHORIZED)
     ;
 
 
-    private final int code;
+    private final int code; // future use
     private final String message;
+    private final HttpStatusCode statusCode;
 }
